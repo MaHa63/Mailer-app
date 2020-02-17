870ef860833853490b6470bba5f7581d7e8fd2b8
MaHa63 <matti.harsu@gmail.com>
2020-02-17 05:57:28 -0500

    Contact functionality

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,14 @@
+class ContactMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.contact_mailer.welcome.subject
+  #
+  def welcome(contact)
+    @contact = contact
+   
+    mail(to: @contact.email, subject: "Welcome to our company")
+  end
+
+end
