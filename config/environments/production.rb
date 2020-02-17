dfa9a8eec5b8cf8a83cb08742cf9560a45a9a9e4
MaHa63 <matti.harsu@gmail.com>
2020-02-17 11:23:45 -0500

    more debugging

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,4 +95,5 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp 
   config.action_mailer.default_url_options = { :host => 'matti-mailer.herokuapp.com', :protocol => 'https' }
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
 end
